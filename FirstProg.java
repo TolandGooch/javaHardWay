63827f773d19a00f38b18057e0eea016843d682f
Toland Gooch <TolandG@Tolands-MacBook-Pro.local>
2016-06-15 13:41:31 -0600

    addexercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg { 
+	public static void main( String [] args ){
+		System.out.println( "I, Toland Gooch, am determined to learn how to code." );
+		System.out.println( "Today's date is Wednesday, June 6, 2016. " );
+	}
+}
