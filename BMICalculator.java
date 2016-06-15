aff252d70b343a7342e019288bdfbdcac5aa4872
Toland Gooch <TolandG@Tolands-MacBook-Pro.local>
2016-06-15 13:52:48 -0600

    add exercise 9

---
+++ BMICalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+	public static void main( String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		double m, kg, bmi;
+		
+		System.out.print( "Your height (feet only): " );
+		m = keyboard.nextDouble();
+		
+		ystem.out.print( "Your height (inches): " );
+		m = keyboard.nextDouble();
+		
+		System.out.print( "Your weight in pounds: " );
+		kg = keyboard.nextDouble();
+		
+		bmi = kg / (m*m);
+		
+		System.out.println( " Your BMI is " + bmi );
+	}
+}
