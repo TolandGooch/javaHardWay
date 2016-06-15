57129a07ddb994db6a4052cde5a390efee0529d2
Toland Gooch <tolandgooch@gmail.com>
2016-06-15 13:56:43 -0600

    Update GasolineReceipt.java

--- GasolineReceipt.java
+++ GasolineReceipt.java
@@ -3,7 +3,7 @@ public class GasolineReceipt {
 		System.out.println( " +--------------------------+" );
 		System.out.println( " |                          |" );
 		System.out.println( " |     Corner Store         |" );
-		System.out.println(	" |                          |" );
+		System.out.println( " |                          |" );
 		System.out.println( " | 2015-03-29   04:38PM     |" );
 		System.out.println( " |                          |" );
 		System.out.println( " | Gallons:        10.870   |" );
